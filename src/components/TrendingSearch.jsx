37a71136caab8d1ab445252f588e9298baf9cd4f
Yashwanth Uppu <Yashwanth.u@EIS.LOCAL>
2025-10-27 15:34:22 +0530

    Add new component

---
+++ src/components/TrendingSearch.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function TrendingSearch(){
+    <>
+    <div className="Trending-search">
+        <div><h1>Trending Searches right now</h1></div>
+        <div>
+            <h4 style={{color:"red"}}>Mass Jathara</h4>
+        </div>
+    </div>
+    </>
+}
+export default TrendingSearch;
